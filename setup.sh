53ab83da19f7d1521ff19a7275a174cdae95f379
matchs <contato@mateuschagas.com.br>
2014-07-22 20:22:19 -0300

    Adding delete command. Using application name on pm2 actions

--- setup.sh
+++ setup.sh
@@ -8,9 +8,12 @@ case $1 in
       node_modules/pm2/bin/pm2 start server.js -i max --name JusBrasil-Oneformat
       ;;
   stop) 
-      node_modules/pm2/bin/pm2 stop server.js
+      node_modules/pm2/bin/pm2 stop JusBrasil-Oneformat
       ;;
   restart)
-      node_modules/pm2/bin/pm2 restart server.js
+      node_modules/pm2/bin/pm2 restart JusBrasil-Oneformat
+      ;;
+  delete)
+      node_modules/pm2/bin/pm2 delete JusBrasil-Oneformat
       ;;
 esac
