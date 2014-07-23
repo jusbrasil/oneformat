998dbf808973cdfc74ce2934e7b0c2d9692900ed
matchs <contato@mateuschagas.com.br>
2014-07-23 10:11:17 -0300

    Removing cookboks

--- chef/cookbooks/oneformat/recipes/dependencies.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: oneformat 
-# Recipe:: dependencies
-
-include_recipe "runit"
-include_recipe "nodejs::install_from_source"
-include_recipe "nodejs::npm"
-include_recipe "git"
-
