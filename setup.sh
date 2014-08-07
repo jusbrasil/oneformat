3a02971a4d474cda2fddd95ff6cd688718a7e16e
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2014-08-06 21:14:00 -0300

    The setup script accept multiples parameters

--- setup.sh
+++ setup.sh
@@ -5,23 +5,25 @@ SERVICE="JusBrasil-Oneformat"
 
 RUN_CMD="$PM2 start server.js -i max --name $SERVICE"
 
-case $1 in
-  install)
-      npm install
-      ;;
-  run)
-      $RUN_CMD
-      ;;
-  run-foreground)
-      $RUN_CMD --no-daemon
-      ;;
-  stop) 
-      $PM2 stop $SERVICE
-      ;;
-  restart)
-      $PM2 restart $SERVICE
-      ;;
-  delete)
-      $PM2 delete $SERVICE
-      ;;
-esac
+for arg in "$@"; do
+  case $arg in
+    install)
+        npm install
+        ;;
+    run)
+        $RUN_CMD
+        ;;
+    run-foreground)
+        $RUN_CMD --no-daemon
+        ;;
+    stop) 
+        $PM2 stop $SERVICE
+        ;;
+    restart)
+        $PM2 restart $SERVICE
+        ;;
+    delete)
+        $PM2 delete $SERVICE
+        ;;
+  esac
+done
