76c5602bb6adc5c1bc3f31afb4a5a5b610ab593a
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2014-08-06 21:12:55 -0300

    Add option on setup script run srv in foreground

--- setup.sh
+++ setup.sh
@@ -3,12 +3,17 @@
 PM2="node_modules/pm2/bin/pm2"
 SERVICE="JusBrasil-Oneformat"
 
+RUN_CMD="$PM2 start server.js -i max --name $SERVICE"
+
 case $1 in
   install)
       npm install
       ;;
   run)
-      $PM2 start server.js -i max --name $SERVICE
+      $RUN_CMD
+      ;;
+  run-foreground)
+      $RUN_CMD --no-daemon
       ;;
   stop) 
       $PM2 stop $SERVICE
