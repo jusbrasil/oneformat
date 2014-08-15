0e6ea42423a8ee5a7b18fa15d6f5152abe25fd51
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2014-08-15 14:46:35 -0300

    Allow POST and PUT request

--- server.js
+++ server.js
@@ -8,24 +8,29 @@ var app = express();
 
 app.listen(config.port);
 
-app.put('/format', function(req, res, next){
-    var form = new multiparty.Form();
+function oneformat(req, res, next){
+  var form = new multiparty.Form();
 
-    form.parse(req, function(err, fields, files) {
-        if(err) next(err);
-        else if(!fields.body) {
-            res.status(400).send("You gotta send me some body to parse");
-            return next();
-        } else {
-            fields.body = fields.body[0];
-            fields.title = (fields.title && fields.title[0]) || '';
-            parser.parse(fields, function(result, err){
-                if(err) next(err);
-                else {
-                    res.send(result);
-                    return next();
-                }
-            });
-        }
-    });
-});
+  form.parse(req, function(err, fields, files) {
+      if(err) next(err);
+      else if(!fields.body) {
+          res.status(400).send("You gotta send me some body to parse");
+          return next();
+      } else {
+          fields.body = fields.body[0];
+          fields.title = (fields.title && fields.title[0]) || '';
+          parser.parse(fields, function(result, err){
+              if(err) next(err);
+              else {
+                  res.send(result);
+                  return next();
+              }
+          });
+      }
+  });
+};
+
+app.route('/format')
+  .post(oneformat)
+  .put(oneformat);
+
