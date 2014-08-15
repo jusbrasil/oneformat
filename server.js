638ff710e26dacddacbbf64a5694e739e9f65b06
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2014-08-15 15:26:00 -0300

    Add support for multiple request content-type
     - Add application/json
     - Add application/x-www-form-urlencoded
     - Keep multipart/form-data
     - Migrated from multiparty to multer

--- server.js
+++ server.js
@@ -2,35 +2,38 @@ var express = require('express'),
     config = require('./config.json'),
     parser = require('./modules/parser/' + config.parser),
     promise = require('promise'),
-    multiparty = require('multiparty');
+    multer  = require('multer'),
+    bodyParser = require('body-parser');
+
 
 var app = express();
+
+// parse multipart/form-data
+app.use(multer())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 app.listen(config.port);
 
 function oneformat(req, res, next){
-  var form = new multiparty.Form();
-
-  form.parse(req, function(err, fields, files) {
-      if(err) next(err);
-      else if(!fields.body) {
-          res.status(400).send("You gotta send me some body to parse");
-          return next();
-      } else {
-          fields.body = fields.body[0];
-          fields.title = (fields.title && fields.title[0]) || '';
-          parser.parse(fields, function(result, err){
-              if(err) next(err);
-              else {
-                  res.send(result);
-                  return next();
-              }
-          });
-      }
-  });
+    if(!req.body.body) {
+        res.status(400).send("You gotta send me some body to parse");
+        return next();
+    } else {
+        var fields = req.body;
+        if(!fields.title) fields.title = "";
+        parser.parse(fields, function(result, err){
+            if(err) next(err);
+            else {
+                res.send(result);
+                return next();
+            }
+        });
+    }
 };
 
 app.route('/format')
-  .post(oneformat)
-  .put(oneformat);
-
+   .post(oneformat)
+   .put(oneformat);
