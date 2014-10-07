71d56313bcca36eb0e7786666b0164d7fb9e41c5
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2014-10-07 15:50:01 -0300

    Add UUID to track the request

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express'),
     config = require('./config.json'),
     parser = require('./modules/parser/' + config.parser),
     multer  = require('multer'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    uuid = require('node-uuid');
 
 
 var app = express();
@@ -17,15 +18,24 @@ app.use(bodyParser.json())
 app.listen(config.port);
 
 function oneformat(req, res, next){
+    var requestId = uuid.v1();
     if(!req.body.body) {
-        res.status(400).send("You gotta send me some body to parse");
+        res.status(400).send({
+            uuid: requestId,
+            message: 'You gotta send me some body to parse'
+        });
         return next();
     } else {
         var fields = req.body;
         if(!fields.title) fields.title = "";
         parser.parse(fields, function(result, err){
-            if(err) next(err);
-            else {
+            if (err) {
+                res.status(500).send({
+                    uuid: requestId,
+                    message: 'Can not parser the content'
+                });
+            } else {
+                result['uuid'] = requestId;
                 res.send(result);
                 return next();
             }
